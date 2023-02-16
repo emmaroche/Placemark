5beab57b5fcc135a5e39b173a9c6458a67b0350c
emmaroche <20088680@mail.wit.ie>
2023-02-16 16:47:26 +0000

    Complete Lab-A03 Placemark-03 (exercises not included)

--- app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
@@ -9,30 +9,45 @@ import ie.setu.placemark.R
 import ie.setu.placemark.databinding.ActivityPlacemarkBinding
 import ie.setu.placemark.main.MainApp
 import ie.setu.placemark.models.PlacemarkModel
+import timber.log.Timber.i
 
 class PlacemarkActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityPlacemarkBinding
     var placemark = PlacemarkModel()
+    //val placemarks = ArrayList<PlacemarkModel>()
     lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityPlacemarkBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.toolbarAdd.title = title
         setSupportActionBar(binding.toolbarAdd)
+
         app = application as MainApp
+
+        i("Placemark Activity started...")
+
+        if (intent.hasExtra("placemark_edit")) {
+            placemark = intent.extras?.getParcelable("placemark_edit")!!
+            binding.placemarkTitle.setText(placemark.title)
+            binding.description.setText(placemark.description)
+        }
 
         binding.btnAdd.setOnClickListener() {
             placemark.title = binding.placemarkTitle.text.toString()
-            placemark.desc = binding.description.text.toString()
+            placemark.description = binding.description.text.toString()
             if (placemark.title.isNotEmpty()) {
-                app.placemarks.add(placemark.copy())
+                app.placemarks.create(placemark.copy())
+                i("add Button Pressed: ${placemark}")
                 setResult(RESULT_OK)
                 finish()
             }
             else {
-                Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                Snackbar
+                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
                     .show()
             }
         }
@@ -45,7 +60,9 @@ class PlacemarkActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.item_cancel -> { finish() }
+            R.id.item_cancel -> {
+                finish()
+            }
         }
         return super.onOptionsItemSelected(item)
     }
--- app/src/main/java/ie/setu/placemark/main/MainApp.kt
+++ app/src/main/java/ie/setu/placemark/main/MainApp.kt
@@ -1,20 +1,17 @@
 package ie.setu.placemark.main
 
 import android.app.Application
-import ie.setu.placemark.models.PlacemarkModel
+import ie.setu.placemark.models.PlacemarkMemStore
 import timber.log.Timber
 import timber.log.Timber.i
 
 class MainApp : Application() {
 
-    val placemarks = ArrayList<PlacemarkModel>()
+    val placemarks = PlacemarkMemStore()
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         i("Placemark started")
-        placemarks.add(PlacemarkModel("One", "About one..."))
-        placemarks.add(PlacemarkModel("Two", "About two..."))
-        placemarks.add(PlacemarkModel("Three", "About three..."))
     }
 }
